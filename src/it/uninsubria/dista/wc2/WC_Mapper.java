cd325295083387573c6acb8dd70e699d5affa4ca
Mirko <mirkox90@gmail.com>
2023-03-28 14:00:20 +0200

    first commit

---
+++ src/it/uninsubria/dista/wc2/WC_Mapper.java
@@ -0,0 +1,29 @@
+package it.uninsubria.dista.wc2;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+
+
+public class WC_Mapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+	private static final IntWritable one = new IntWritable(1);
+	private Text word = new Text();
+
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+		String line = value.toString();
+		String[] cols = line.split(',');
+		String make = cols[6];
+		String model = cols[7];
+
+		word.set(make + " " + model);
+		context.write(word, one);
+	}
+
+}
